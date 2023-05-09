dba63c2d94d756fc45a4183b085490fac4a5e115
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-09 19:33:06 -0300

    Desafio 13 concluido, criação função hydrate

--- src/challenges2.js
+++ src/challenges2.js
@@ -40,35 +40,30 @@ const generatePhoneNumber = (arrayNumber) => {
 // Desafio 12 -  Crie a função triangleCheck
 const triangleCheck = (lineA, lineB, lineC) => {
   const lineValuesObj = {
-    A:{
-      value: lineA,
-      soma:lineB + lineC,
-      sub: Math.abs(lineB - lineC),
-    },
-    B:{
-      value: lineB,
-      soma: lineA + lineC,
-      sub: Math.abs(lineA - lineC),
-    },
-    C:{
-      value: lineC,
-      soma: lineA + lineB,
-      sub: Math.abs(lineA - lineB),
-    },
-  }
+    A: { value: lineA, soma: lineB + lineC, sub: Math.abs(lineB - lineC) },
+    B: { value: lineB, soma: lineA + lineC, sub: Math.abs(lineA - lineC) },
+    C: { value: lineC, soma: lineA + lineB, sub: Math.abs(lineA - lineB) },
+  };
 
   for (let key of Object.keys(lineValuesObj)) {
-    if (lineValuesObj[key].value > lineValuesObj[key].soma ||
-        lineValuesObj[key].value < lineValuesObj[key].sub) {
-      return false
+    if (lineValuesObj[key].value > lineValuesObj[key].soma
+      || lineValuesObj[key].value < lineValuesObj[key].sub) {
+      return false;
     }
   }
-  return true
-}
-
-console.log(triangleCheck(2, 3, 4));
+  return true;
+};
 
 // Desafio 13 - Crie a função hydrate
+const hydrate = (phrase) => {
+  let pattern = /[0-9]/g;
+  let onlyNumbers = phrase.match(pattern);
+  let soma = 0;
+  for (let number of onlyNumbers) {
+    soma += Number(number);
+  }
+  return (soma === 1) ? `${soma} copo de água` : `${soma} copos de água`;
+};
 
 /* eslint no-undef: 0 */
 
